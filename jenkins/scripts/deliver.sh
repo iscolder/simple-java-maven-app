336b9184846766dc1024512288ea0d5e3cae0245
Konstantin <kemelyanov@evelopers.com>
2022-11-30 20:31:53 +0100

    updated script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -7,6 +7,7 @@ echo 'volume).'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
+#123
 
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
